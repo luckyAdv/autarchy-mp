629cf538e1249ddee54e742d20da74d8a001e056
luckyAdv <166721659+luckyAdv@users.noreply.github.com>
2024-05-05 14:32:01 -0700

    rename serverlogo

--- client_packages/hud/hud.js
+++ client_packages/hud/hud.js
@@ -1,3 +1,3 @@
 mp.events.add('render', () => {
-    mp.game.graphics.drawText("GTA.FAMZONE.RO", [0.93, 0.97], {font: 7, color: [255, 255, 255, 255], scale: [0.5, 0.5], outline: true}); // LOGO SERVER
+    mp.game.graphics.drawText("Autarchy MP", [0.93, 0.97], {font: 7, color: [255, 255, 255, 255], scale: [0.5, 0.5], outline: true}); // LOGO SERVER
 });
